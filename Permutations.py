84f25b460ee763a0ac695e79faa9f3afc6ac19c4
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-03-01 23:28:09 -0700

    Create Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,14 @@
+#46. Permutations
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def permutation(i,nums,answer):
+            if i == len(nums) - 1:
+                answer.append(nums[:])
+            else:
+                for j in range(i,len(nums)):
+                    nums[i],nums[j] = nums[j],nums[i]
+                    permutation(i+1,nums,answer)
+                    nums[i],nums[j] = nums[j],nums[i]
+        answer = []
+        permutation(0,nums,answer)
+        return answer
