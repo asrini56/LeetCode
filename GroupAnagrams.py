6498d34d2445deffd185455d2fe336eb9d9a5ea0
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-03-03 21:55:30 -0700

    Create GroupAnagrams.py

---
+++ GroupAnagrams.py
@@ -0,0 +1,13 @@
+#49. Group Anagrams, Time-O(n)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = {}
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            if tuple(count) not in ans:
+                ans[tuple(count)] = [s]
+            else:
+                ans[tuple(count)].append(s)
+        return ans.values()
