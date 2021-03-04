0cc07fbeb0bf2b6e3e373f4ff619bd3034c03fc4
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-03-03 21:39:30 -0700

    Create SnapshotArray.py

---
+++ SnapshotArray.py
@@ -0,0 +1,26 @@
+#1146. Snapshot Array
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.cache = []
+        self.d = {}
+        self.i = 0
+
+    def set(self, index: int, val: int) -> None:
+        self.d[index] = val
+
+    def snap(self) -> int:
+        self.cache.append(dict(self.d))
+        self.i += 1
+        return self.i-1
+
+    def get(self, index: int, snap_id: int) -> int:
+        snap = self.cache[snap_id]
+        return snap[index] if index in snap else 0
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
