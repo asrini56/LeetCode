b1de2cebc1d2cd3b3499a6d76d505b0bd470e74f
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-03-05 20:15:19 -0700

    Create TaskScheduler.py

---
+++ TaskScheduler.py
@@ -0,0 +1,29 @@
+#621. Task Scheduler, Time - O(n)
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord('A')]+=1
+        freq.sort()
+        fmax = freq.pop()
+        idle_time = (fmax - 1) * n
+        
+        while idle_time and freq:
+            print(len(freq),idle_time)
+            idle_time -= min(freq.pop(),fmax-1)
+        idle_time = max(0,idle_time)
+        return idle_time + len(tasks)
+    
+    """
+    working:
+    tasks = ["A","A","A","B","B","B"], n = 2
+    freq = [3,3]
+    freq.sort() = [0,0,.....3,3]
+    fmax = 3
+    idle = 4 [(3-1)*2]
+    while idle and freq:
+        1] idle -= min(3,2) = idle = 2
+        ...
+    idle = max(idle,0) = 2
+    ans = 2 + 6 = 8
+    """
