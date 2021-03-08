f8993916a8bfc5bc574c1c1d8a3c48351ca140f9
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-03-07 21:02:18 -0700

    Update LongestArithSub.py

--- LongestArithSub.py
+++ LongestArithSub.py
@@ -1,8 +1,12 @@
 #1027. Longest Arithmetic Subsequence, Time - O(n^2)
 class Solution:
     def longestArithSeqLength(self, A: List[int]) -> int:
-        dp = {}
-        for i in range(len(A)):
-            for j in range(i+1,len(A)):
-                dp[j,A[j] - A[i]] = dp.get((i, A[j] - A[i]), 1) + 1
-        return max(dp.values())
+        d = {}
+        for i in range(1,len(A)):
+            for j in range(0,i):
+                diff = A[i] - A[j]
+                if (j,diff) in d:
+                    d[i,diff] = d[j,diff] + 1
+                else:
+                    d[i,diff] = 2
+        return max(d.values())
