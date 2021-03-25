acbd277c4f50779e58f6cab6538d552f7b813b10
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-03-24 21:07:08 -0700

    Create TwoCityScheduling.py

---
+++ TwoCityScheduling.py
@@ -0,0 +1,9 @@
+#1029. Two City Scheduling, Time - O(NlogN)
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        cost = 0
+        costs.sort(key = lambda x:x[0]-x[1])
+        n = len(costs) // 2
+        for i in range(n):
+            cost += costs[i][0] + costs[i+n][1]
+        return cost
