6dfd577e76de5e2eb19a50aa65c7f941b1b2e5cb
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-05-18 15:16:44 -0700

    Update ConcatenatedWords.py

--- ConcatenatedWords.py
+++ ConcatenatedWords.py
@@ -1,44 +1,49 @@
 #472. Concatenated Words
-import collections
+"""
+time complexity would be m * w * n ^2
+where,
+m = number of words in concatenated words
+w = maximum length of word in concatenated words
+n = length of words
 
-class TrieNode:
-    def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
-        self.isEnd = False
+"""
+import collections
 
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = {}
+        self.endSymbol = "*"
     
-    def insert(self, word):
-        marker = self.root
-        for ch in word:
-            marker = marker.children[ch]
-        marker.isEnd = True
-
+    def addWords(self,word):
+        node = self.root
+        for w in word:
+            if w not in node:
+                node[w] = {}
+            node = node[w]
+        node[self.endSymbol] = True
 
 class Solution:
     def findAllConcatenatedWordsInADict(self, words):
         
-        def dfs(word, start, root, count):
-            n = len(word)
-            marker = root
-            for i in range(start, n):
-                marker = marker.children[word[i]]
-                if marker.isEnd:           # smaller word encountered
-                    if i == n-1:               # leaf node
-                        return count>=1
-                    elif dfs(word, i+1, root, count+1):     # increment the count and start a new DFS
-                        return True
+        def dfs(word,start,root,count,endSymbol):
+            #Search a Word in Trie
+            node = root
+            for i in range(start,len(word)):
+                if word[i] in node:
+                    node = node[word[i]]
+                    if endSymbol in node:
+                        if i == len(word)-1:
+                            return count >= 1
+                        elif dfs(word,i+1,root,count+1,endSymbol):
+                            return True
+                else:
+                    break
             return False
-        
-        res = []
         trie = Trie()
         for word in words:
-            trie.insert(word)
-
+            trie.addWords(word)
+        ans = []
         for word in words:
-            if dfs(word, 0, trie.root, 0):
-                res += word,
-        
-        return res
+            if dfs(word,0,trie.root,0,trie.endSymbol):
+                ans.append(word)
+        return ans
