9a45fa8bfc082941671afc0a9e470fb2137c45fb
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-05-19 14:27:24 -0700

    Update LFUCache.py

--- LFUCache.py
+++ LFUCache.py
@@ -1,61 +1,36 @@
+#460. LFU Cache, Time - O(1)
 class LFUCache:
     import collections
     def __init__(self, capacity: int):
-        self.maximum = capacity
-        self.time = 0
-        self.keyValue = collections.OrderedDict()
-        self.freq = collections.OrderedDict()
-        self.check = collections.OrderedDict()
+        self.capacity = capacity
+        self.freq_to_key = defaultdict(OrderedDict)
+        self.key_to_freq = defaultdict(int)
+        self.min_freq = 1
+        
 
     def get(self, key: int) -> int:
-        if self.maximum == 0:
+        if key not in self.key_to_freq:
             return -1
-        self.time+=1
-        if key not in self.keyValue.keys():
-            return -1
-        else:
-            if key in self.freq.keys():
-                self.freq[key]+=1
-            if key in self.check:
-                del self.check[key]
-            self.check[key] = self.time
-            return self.keyValue[key]
+        freq = self.key_to_freq[key]
+        self.key_to_freq[key] = freq + 1
+        value = self.freq_to_key[freq][key]
+        del self.freq_to_key[freq][key]
+        self.freq_to_key[freq+1][key] = value
+        if self.min_freq == freq and not self.freq_to_key[freq]:
+            self.min_freq += 1
+        return value
+        
 
     def put(self, key: int, value: int) -> None:
-        if self.maximum != 0:
-            self.time+=1
-            if len(self.keyValue) < self.maximum or key in self.keyValue.keys():
-                if key in self.check.keys():
-                    del self.check[key]
-                self.check[key] = self.time
-                if key in self.freq.keys():
-                    self.freq[key]+=1
-                else:
-                    self.freq[key] = 1
-                self.keyValue[key] = value
-            if len(self.keyValue) == self.maximum and key not in self.keyValue.keys():
-                k = min(self.freq.values())
-                res = [key for key in self.freq if self.freq[key] == k]
-                if len(res) > 1:
-                    m = self.check[res[0]]
-                    index = res[0]
-                    for i in range(len(res)):
-                        if self.check[res[i]] < m:
-                            index = res[i]
-                            m = self.check[res[i]]
-                    del self.keyValue[index]
-                    del self.check[index]
-                    del self.freq[index]
-                else:
-                    del self.keyValue[res[0]]
-                    del self.check[res[0]]
-                    del self.freq[res[0]]
-                self.keyValue[key] = value
-                self.check[key] = self.time
-                self.freq[key] = 1
-
-
-# Your LFUCache object will be instantiated and called as such:
-# obj = LFUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        if key in self.key_to_freq:
+            self.get(key)
+            self.freq_to_key[self.key_to_freq[key]][key] = value
+        else:
+            self.capacity -= 1
+            self.key_to_freq[key] = 1
+            self.freq_to_key[1][key] = value
+            if self.capacity < 0:
+                self.capacity += 1
+                k, v = self.freq_to_key[self.min_freq].popitem(False)
+                del self.key_to_freq[k]
+            self.min_freq = 1
