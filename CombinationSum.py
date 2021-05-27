3b7cbeefb01558f4204baa67b0695afe4cf27e01
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-05-26 19:35:42 -0700

    Update CombinationSum.py

--- CombinationSum.py
+++ CombinationSum.py
@@ -1,4 +1,8 @@
 #39. Combination Sum
+"""
+Let N be the number of candidates, T be the target value, and M be the minimal value among the candidates.
+Time - O(n^T/M +1), SPACE - O(T/M) RECURSION DEPTH
+"""
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
