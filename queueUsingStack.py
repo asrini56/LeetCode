34778f39a8d5b02f6f457956f86a363ddd3514c9
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2021-06-14 17:31:37 -0700

    Create queueUsingStack.py

---
+++ queueUsingStack.py
@@ -0,0 +1,41 @@
+#time - O(1) amortized
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.master = []
+        self.slave = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.slave.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.master:
+            self.move()
+        return self.master.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.master:
+            self.move()
+        return self.master[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not len(self.master) and not len(self.slave)
+
+    def move(self) -> None:
+        while self.slave:
+            self.master.append(self.slave.pop())
