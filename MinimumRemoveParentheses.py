0d28892614a8e49e245c7d360107e65cd13daa93
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-01-01 04:17:34 -0500

    Update MinimumRemoveParentheses.py

--- MinimumRemoveParentheses.py
+++ MinimumRemoveParentheses.py
@@ -1,21 +1,16 @@
 #1249. Minimum Remove to Make Valid Parentheses, Time - O(n)
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        index = set()
-        st = []
-        for i in range(len(s)):
-            if s[i] != '(' and s[i] != ')':
-                continue
-            if s[i] == '(':
-                st.append(i)
-            elif not st:
-                index.add(i)
-            else:
-                st.pop()
-        while st:
-            index.add(st.pop())
-        ans = []
-        for i in range(len(s)):
-            if i not in index:
-                ans.append(s[i])
-        return ''.join(ans)
+        s = list(s)
+        stack = []
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+        while stack:
+            s[stack.pop()] = ''
+        return ''.join(s)
