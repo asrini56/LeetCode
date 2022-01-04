6524d5d730090ae34ab13ef19eb779ba7b1da577
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-01-04 01:11:01 -0500

    Create Mergetwosortedarray.py

---
+++ Mergetwosortedarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, n1: List[int], m: int, n2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if n1[m-1] > n2[n-1]:
+                n1[m+n-1] = n1[m-1]
+                m-=1
+            else:
+                n1[m+n-1] = n2[n-1]
+                n-=1
+        #if n1 is empty, n1 = n2 , n will remain same and m as 0
+        n1[:n] = n2[:n]
