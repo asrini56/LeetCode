936b2957cc6eaa330ce97498208db9ce6a155f2f
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-01-18 22:58:01 -0500

    Update ContinuousSubarraySum.py

--- ContinuousSubarraySum.py
+++ ContinuousSubarraySum.py
@@ -1,11 +1,15 @@
-#Time - O(N)
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         """
+        
+        We are trying to find two numbers which modulo to same number
+        the difference of their sum will always modulo to 0
         d = {0:-1}
         d = {0:-1,5:0}
         d = {0:-1,5:0,1:1}
         d = {0:-1,5:0,1:1} -> True [5 present in d]
+        
+        say the the difference is d between a and b, such as d = b - a(b is on the right of a). you want d is multiple of k, so you just need d % k = 0. Because d = b - a, so d % k = 0 = (b - a) %k. so (b-a)%k=0 equal b%k - a%k = 0, then b%k = a%k. Comparing other hashtable based problem, you need check b-k whether in the hashtable. In this problem , you always check b%k, and always pust a%k into hashtable. when k = 0, you need do it as other similar problem.
         """
         if len(nums) < 2:
             return False
@@ -21,3 +25,5 @@ class Solution:
             else:
                 d[s] = i
         return False
+        
+            
