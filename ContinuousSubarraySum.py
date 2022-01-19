c502f3babea396b4f955aa2af3275e6935c5ea46
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-01-18 22:58:27 -0500

    Update ContinuousSubarraySum.py

--- ContinuousSubarraySum.py
+++ ContinuousSubarraySum.py
@@ -9,7 +9,11 @@ class Solution:
         d = {0:-1,5:0,1:1}
         d = {0:-1,5:0,1:1} -> True [5 present in d]
         
-        say the the difference is d between a and b, such as d = b - a(b is on the right of a). you want d is multiple of k, so you just need d % k = 0. Because d = b - a, so d % k = 0 = (b - a) %k. so (b-a)%k=0 equal b%k - a%k = 0, then b%k = a%k. Comparing other hashtable based problem, you need check b-k whether in the hashtable. In this problem , you always check b%k, and always pust a%k into hashtable. when k = 0, you need do it as other similar problem.
+        say the the difference is d between a and b, such as d = b - a(b is on the right of a). 
+        you want d is multiple of k, so you just need d % k = 0. Because d = b - a, so d % k = 0 = (b - a) %k. 
+        so (b-a)%k=0 equal b%k - a%k = 0, then b%k = a%k. 
+        Comparing other hashtable based problem, you need check b-k whether in the hashtable. 
+        In this problem , you always check b%k, and always pust a%k into hashtable. when k = 0, you need do it as other similar problem.
         """
         if len(nums) < 2:
             return False
