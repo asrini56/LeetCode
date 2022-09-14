203babce57c22d2c9900741f4a523eb27b574fda
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-09-14 17:44:12 -0400

    Create pseudoPalindromicMap.py

---
+++ pseudoPalindromicMap.py
@@ -0,0 +1,36 @@
+def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        
+        freq = [0 for _ in range(10)]
+        res = 0
+        
+        def isPalindrome():
+            count_odd = 0
+            for i in range(10):
+                if freq[i] % 2:
+                    count_odd += 1
+            
+            return False if count_odd > 1 else True
+            
+        
+        def dfs(node):
+            
+            nonlocal res
+            
+            if not node:
+                return
+            
+            freq[node.val] += 1
+            
+            if not node.left and not node.right:
+                if isPalindrome():
+                    res += 1
+            else:
+                dfs(node.left)
+                dfs(node.right)
+            
+            freq[node.val] -= 1
+            
+        
+        dfs(root)
+        
+        return res
