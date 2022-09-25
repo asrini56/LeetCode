d4901a9c09a6d837acd2b7708056cfc6a8e3339e
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-09-24 22:27:31 -0400

    Create findduplicatenumber.py

---
+++ findduplicatenumber.py
@@ -0,0 +1,15 @@
+Time - O(n), Space - O(1)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for num in nums:
+            cur = abs(num)
+            if nums[cur] < 0:
+                duplicate = cur
+                break
+            nums[cur] = -nums[cur]
+
+        # Restore numbers
+        for i in range(len(nums)):
+            nums[i] = abs(nums[i])
+
+        return duplicate
