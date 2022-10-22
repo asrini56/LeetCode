39ad3e7257c7c14d72fc67e0aa8ddb4f615fe6de
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-10-22 19:04:38 -0400

    Update FindMinRotatedArr.py

--- FindMinRotatedArr.py
+++ FindMinRotatedArr.py
@@ -1,12 +1,19 @@
 #153. Find Minimum in Rotated Sorted Array, Time - O(logn)
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        lo = 0
-        hi = len(nums)-1
-        while (lo < hi):
-            mid = lo + (hi-lo)//2
-            if (nums[mid] < nums[hi]):
-                hi = mid;
+        if len(nums) == 1:
+            return nums[0]
+        left = 0
+        right = len(nums)-1
+        if nums[right] > nums[0]:
+            return nums[0]
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+            if nums[mid-1] > nums[mid]:
+                return nums[mid]
+            if nums[mid] > nums[0]:
+                left = mid + 1
             else:
-                lo = mid+1;
-        return nums[lo]
+                right = mid - 1
