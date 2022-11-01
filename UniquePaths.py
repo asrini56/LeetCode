72eba69aadbd02b82158c2223fc092e4111b8946
ASHWIN SRINIVASAN <54925408+asrini56@users.noreply.github.com>
2022-10-31 21:03:44 -0400

    Update UniquePaths.py

--- UniquePaths.py
+++ UniquePaths.py
@@ -6,3 +6,13 @@ class Solution:
             for j in range(1,n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
+
+    
+    
+#Unique Paths, Time - O(n*m), Space - O(m)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1]*n
+        for _, j in product(range(1, m), range(1, n)):
+            dp[j] += dp[j-1]
+        return dp[-1]
